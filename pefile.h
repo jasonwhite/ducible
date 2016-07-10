5111cabb9cd9d4e5187ac60fb50e5d845718e464
Jason White <jasonaw0@gmail.com>
2016-07-10 03:26:32 -0700

    Add initial code to read image files

--- pefile.h
+++ pefile.h
@@ -18,6 +18,42 @@ extern "C" {
 #pragma pack(push, 4)
 
 //
+// Image Format
+//
+
+#define IMAGE_DOS_SIGNATURE                 0x5A4D      // MZ
+#define IMAGE_OS2_SIGNATURE                 0x454E      // NE
+#define IMAGE_OS2_SIGNATURE_LE              0x454C      // LE
+#define IMAGE_VXD_SIGNATURE                 0x454C      // LE
+#define IMAGE_NT_SIGNATURE                  0x00004550  // PE00
+
+#pragma pack(push, 2) // 16 bit headers are 2 byte packed
+
+typedef struct _IMAGE_DOS_HEADER {      // DOS .EXE header
+    uint16_t e_magic;    // Magic number
+    uint16_t e_cblp;     // Bytes on last page of file
+    uint16_t e_cp;       // Pages in file
+    uint16_t e_crlc;     // Relocations
+    uint16_t e_cparhdr;  // Size of header in paragraphs
+    uint16_t e_minalloc; // Minimum extra paragraphs needed
+    uint16_t e_maxalloc; // Maximum extra paragraphs needed
+    uint16_t e_ss;       // Initial (relative) SS value
+    uint16_t e_sp;       // Initial SP value
+    uint16_t e_csum;     // Checksum
+    uint16_t e_ip;       // Initial IP value
+    uint16_t e_cs;       // Initial (relative) CS value
+    uint16_t e_lfarlc;   // File address of relocation table
+    uint16_t e_ovno;     // Overlay number
+    uint16_t e_res[4];   // Reserved words
+    uint16_t e_oemid;    // OEM identifier (for e_oeminfo)
+    uint16_t e_oeminfo;  // OEM information; e_oemid specific
+    uint16_t e_res2[10]; // Reserved words
+    int32_t  e_lfanew;   // File address of new exe header
+} IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER;
+
+#pragma pack(pop)
+
+//
 // File header format.
 //
 
