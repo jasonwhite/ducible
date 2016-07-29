6fc9346b3993af8fdd99f11743f68fa1093b2aea
Jason White <jasonaw0@gmail.com>
2016-07-29 00:48:23 -0700

    Add tests to the build

--- BUILD.lua
+++ BUILD.lua
@@ -1,8 +1,31 @@
 local cc = require "rules.cc"
 
-cc.binary {
+local peclean = cc.binary {
     name = "peclean",
     srcs = glob "src/*.cpp",
     warnings = {"all", "error"},
     compiler_opts = {"-g"},
 }
+
+--
+-- Test peclean
+--
+local tests = {
+    {image = "vs/vs2015/Debug/test_dll.dll", pdb = "vs/vs2015/Debug/test_dll.pdb"},
+    {image = "vs/vs2015/Release/test_dll.dll", pdb = "vs/vs2015/Release/test_dll.pdb"},
+    {image = "vs/vs2015/x64/Debug/test_dll.dll", pdb = "vs/vs2015/x64/Debug/test_dll.pdb"},
+    {image = "vs/vs2015/x64/Release/test_dll.dll", pdb = "vs/vs2015/x64/Release/test_dll.pdb"},
+
+    {image = "vs/vs2015/Debug/test_exe.exe", pdb = "vs/vs2015/Debug/test_exe.pdb"},
+    {image = "vs/vs2015/Release/test_exe.exe", pdb = "vs/vs2015/Release/test_exe.pdb"},
+    {image = "vs/vs2015/x64/Debug/test_exe.exe", pdb = "vs/vs2015/x64/Debug/test_exe.pdb"},
+    {image = "vs/vs2015/x64/Release/test_exe.exe", pdb = "vs/vs2015/x64/Release/test_exe.pdb"},
+}
+
+for _,t in ipairs(tests) do
+    rule {
+        inputs = {peclean:path()},
+        task = {{peclean:path(), t.image, t.pdb}},
+        outputs = {},
+    }
+end
