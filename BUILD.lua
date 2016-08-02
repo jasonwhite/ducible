226f7988ef14f21d1d8de4763592cc74fd31aeb9
Jason White <jasonaw0@gmail.com>
2016-08-01 23:03:28 -0700

    Add version.h generation to Button build

--- BUILD.lua
+++ BUILD.lua
@@ -1,8 +1,22 @@
 local cc = require "rules.cc"
 
+-- FIXME: Implicit dependency detection goes a bit overboard on this. Fix this
+-- once there is a way to override dependency detection.
+rule {
+    inputs = glob {
+        "VERSION",
+        "src/version.h.in",
+    },
+    task = {{"./scripts/version.py", "src/version.h.in", "src/version.h"}},
+    outputs = {"src/version.h"},
+}
+
 local pepatch = cc.binary {
     name = "pepatch",
     srcs = glob { "src/*.cpp", "src/*.c" },
+    src_deps = {
+        ["src/main.cpp"] = {"src/version.h"},
+    },
     warnings = {"all", "error"},
     compiler_opts = {"-g"},
 }
