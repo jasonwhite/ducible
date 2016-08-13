8387081e748e59079310f819ac3739edaa435e09
Jason White <jasonaw0@gmail.com>
2016-08-13 16:21:34 -0700

    Add in-memory MSF streams

---
+++ src/msf_memory_stream.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2016 Jason White
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <algorithm>
+#include <cstring>
+
+#include "msf_memory_stream.h"
+
+MsfMemoryStream::MsfMemoryStream(size_t length, const void* buf)
+    : _pos(0)
+{
+    _data.resize(length);
+    memcpy(&_data[0], buf, length);
+}
+
+MsfMemoryStream::MsfMemoryStream(MsfStream* stream)
+    : _pos(0)
+{
+    const size_t length = stream->length();
+
+    _data.resize(length);
+
+    const size_t pos = stream->getPos();
+    stream->setPos(0);
+
+    stream->read(length, &_data[0]);
+
+    stream->setPos(pos);
+}
+
+size_t MsfMemoryStream::length() const {
+    return _data.size();
+}
+
+size_t MsfMemoryStream::getPos() const {
+    return _pos;
+}
+
+void MsfMemoryStream::setPos(size_t pos) {
+    _pos = pos;
+}
+
+size_t MsfMemoryStream::read(size_t length, void* buf) {
+
+    if (_pos >= _data.size())
+        return 0;
+
+    size_t available = std::min(_data.size() - _pos, length);
+
+    memcpy(buf, &_data[0], available);
+
+    _pos += available;
+
+    return available;
+}
+
+size_t MsfMemoryStream::read(void* buf) {
+    return read(_data.size() - _pos, buf);
+}
---
+++ src/msf_memory_stream.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2016 Jason White
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+#include <stdint.h>
+#include <vector>
+
+#include "msf_stream.h"
+
+/**
+ * Represents an MSF file stream.
+ */
+class MsfMemoryStream : public MsfStream {
+private:
+
+    size_t _pos;
+    std::vector<uint8_t> _data;
+
+public:
+    /**
+     * Initialize the stream with an buffer. The buffer is copied internally.
+     *
+     * Params:
+     *   length = Length of the buffer, in bytes.
+     *   buf    = The buffer.
+     */
+    MsfMemoryStream(size_t length, const void* buf);
+
+    /**
+     * Initialize the stream with another stream.
+     */
+    MsfMemoryStream(MsfStream* stream);
+
+    /**
+     * Returns the length of the stream, in bytes.
+     */
+    size_t length() const;
+
+    /**
+     * Gets the current position, in bytes, in the stream.
+     */
+    size_t getPos() const;
+
+    /**
+     * Sets the current position, in bytes, in the stream.
+     */
+    void setPos(size_t p);
+
+    /**
+     * Reads a length of the stream. This abstracts reading from multiple pages.
+     *
+     * Params:
+     *   length = The number of bytes to read from the stream.
+     *   buf    = The buffer to read the stream into.
+     *
+     * Returns: The number of bytes read.
+     */
+    size_t read(size_t length, void* buf);
+
+    /**
+     * Reads the entire stream.
+     *
+     * Params:
+     *   buf = The buffer to read the stream into. This must be large enough to
+     *         hold the entire stream.
+     *
+     * Returns: The number of bytes read.
+     */
+    size_t read(void* buf);
+};
