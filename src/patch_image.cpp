c5047a4b9547e16ae1603831b9dac2190ed91dcb
Jason White <jasonaw0@gmail.com>
2016-09-10 23:20:12 -0700

    Add initial code to patch the /LinkInfo stream
    This includes code to parse the table of named streams in the PDB header stream.

--- src/patch_image.cpp
+++ src/patch_image.cpp
@@ -63,6 +63,9 @@
  * - https://msdn.microsoft.com/en-us/library/ms809762.aspx
  * - http://www.debuginfo.com/articles/debuginfomatch.html
  * - https://github.com/google/syzygy/
+ * - http://llvm.org/svn/llvm-project/llvm/trunk/lib/DebugInfo/PDB/Raw/
+ * - https://github.com/Microsoft/microsoft-pdb (Microsoft's reference
+ *   implementation for reading/writing PDBs.)
  */
 
 #include <stdio.h>
@@ -72,6 +75,7 @@
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <map>
 #include <regex>
 
 #include "patch_image.h"
@@ -257,10 +261,120 @@ void normalizeFileNameGuid(CharT* path, size_t length) {
     }
 }
 
+using NameMapTable = std::map<std::string, uint32_t>;
+
+/**
+ * Reads the name map table in the PDB header stream. This is a map of strings
+ * to stream numbers.
+ *
+ * The format is as follows:
+ *
+ *  1. String buffer:
+ *     (a) stringsLength (4 bytes): The size of the string buffer
+ *     (b) strings (stringsLength bytes): A list of null-terminated strings.
+ *  2. The map of strings to stream indices:
+ *     (a) elemCount (4 byte): The number of items in the map (aka its
+ *         cardinality).
+ *     (b) elemCountMax (4 bytes): The capacity of the map.
+ *     (c) Bitset of present elements. This keeps track of which 'holes' have
+ *         been filled in the map. There should be elemCount bits set in this
+ *         bitset.
+ *         i. count (4 bytes): The number of elements in the bitset
+ *         ii. bitset (count * 4 bytes): The bits
+ *     (c) Bitmap of deleted elements
+ *         i. count (4 bytes): The number of elements in the bitset
+ *         ii. bitset (count * 4 bytes): The bits
+ *     (d) A list of elemCount (string offset, stream index) pairs.
+ *
+ * Microsoft's PDB implementation was used as a reference. More specifically,
+ * see the following files:
+ *
+ *  1. PDB/include/nmtni.h - NMTNI::reload() - for loading the name table from
+ *     disk, which includes a Map.
+ *  2. PDB/include/map.h - Map::reload() - for loading a Map from disk.
+ *  3. PDB/include/iset.h - ISet::reload() - for loading a bitset from disk,
+ *     which is just an Array of longs.
+ */
+NameMapTable readNameMapTable(const uint8_t* data, const uint8_t* dataEnd) {
+    NameMapTable table;
+
+    // Parse the name map
+    if (size_t(dataEnd - data) < sizeof(uint32_t))
+        throw InvalidPdb("missing PDB name table strings length");
+
+    const uint32_t stringsLength = *(const uint32_t*)data;
+    data += sizeof(stringsLength);
+
+    if (size_t(dataEnd - data) < stringsLength)
+        throw InvalidPdb("missing PDB name table strings data");
+
+    // The names of the streams. We'll index into this later.
+    const char* strings = (const char*)data;
+    data += stringsLength;
+
+    if (size_t(dataEnd - data) < 2 * sizeof(uint32_t))
+        throw InvalidPdb("missing PDB stream name map sizes");
+
+    // The number of elements in the hash table.
+    const uint32_t elemCount = *(const uint32_t*)data;
+    data += sizeof(elemCount);
+
+    // The maximum number of elements in the hash table.
+    const uint32_t elemCountMax = *(const uint32_t*)data;
+    data += sizeof(elemCountMax);
+
+    if (size_t(dataEnd - data) < sizeof(uint32_t))
+        throw InvalidPdb("missing PDB name table 'present' bitset size");
+
+    // Skip over the "present" bitset.
+    const uint32_t presentSize = *(const uint32_t*)data;
+    data += sizeof(presentSize);
+    data += presentSize * sizeof(uint32_t);
+
+    if (data > dataEnd)
+        throw InvalidPdb("missing PDB name table 'present' bitset data");
+
+    if (size_t(dataEnd - data) < sizeof(uint32_t))
+        throw InvalidPdb("missing PDB name table 'deleted' bitset size");
+
+    // Skip over the "deleted" bitset.
+    const uint32_t deletedSize = *(const uint32_t*)data;
+    data += sizeof(deletedSize);
+    data += deletedSize * sizeof(uint32_t);
+
+    if (data > dataEnd)
+        throw InvalidPdb("missing PDB name table 'deleted' bitset data");
+
+    if (size_t(dataEnd - data) < elemCount * sizeof(uint32_t) * 2)
+        throw InvalidPdb("missing PDB name table pairs");
+
+    // Finally, read the pairs of string offsets and stream indices
+    const uint32_t* pairs = (const uint32_t*)data;
+    for (size_t i = 0; i < elemCount; ++i) {
+        const uint32_t offset = pairs[i*2];
+
+        if (offset >= stringsLength)
+            throw InvalidPdb("invalid PDB name table offset into strings buffer");
+
+        const char* name = &strings[offset];
+        const uint32_t stream = pairs[i*2+1];
+        table[std::string(name)] = stream;
+    }
+
+    return table;
+}
+
+/**
+ * Patches the LinkInfo named stream.
+ */
+void patchLinkInfoStream(MsfMemoryStream* stream) {
+    // TODO
+}
+
 /**
  * Patches the PDB header stream.
  */
-void patchHeaderStream(MsfMemoryStream* stream, const CV_INFO_PDB70* pdbInfo,
+void patchHeaderStream(MsfFile& msf, MsfMemoryStream* stream, const CV_INFO_PDB70* pdbInfo,
         uint32_t timestamp, const uint8_t signature[16]) {
 
     uint8_t* data = stream->data();
@@ -284,6 +398,23 @@ void patchHeaderStream(MsfMemoryStream* stream, const CV_INFO_PDB70* pdbInfo,
     header->timestamp = timestamp;
     header->age = 1;
     memcpy(header->sig70, signature, sizeof(header->sig70));
+
+    const auto table = readNameMapTable(data, dataEnd);
+
+    // Patch the LinkInfo stream.
+    const auto it = table.find("/LinkInfo");
+    if (it != table.end()) {
+        auto origLinkInfoStream = msf.getStream(it->second);
+        if (!origLinkInfoStream)
+            throw InvalidPdb("missing named LinkInfo stream");
+
+        auto linkInfoStream = std::shared_ptr<MsfMemoryStream>(
+                new MsfMemoryStream(origLinkInfoStream.get()));
+
+        patchLinkInfoStream(linkInfoStream.get());
+
+        msf.replaceStream(it->second, linkInfoStream);
+    }
 }
 
 /**
@@ -526,7 +657,7 @@ void patchPDB(MsfFile& msf, const CV_INFO_PDB70* pdbInfo,
     auto pdbHeaderStream = std::shared_ptr<MsfMemoryStream>(
             new MsfMemoryStream(origPdbHeaderStream.get()));
 
-    patchHeaderStream(pdbHeaderStream.get(), pdbInfo, timestamp, signature);
+    patchHeaderStream(msf, pdbHeaderStream.get(), pdbInfo, timestamp, signature);
 
     msf.replaceStream((size_t)PdbStreamType::header, pdbHeaderStream);
 
