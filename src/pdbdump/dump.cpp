3fe6606d26a18d95929304b26eba7952dbe722e1
Jason White <jasonaw0@gmail.com>
2016-10-24 00:21:00 -0700

    Add DBI header output

--- src/pdbdump/dump.cpp
+++ src/pdbdump/dump.cpp
@@ -38,78 +38,6 @@
 namespace {
 
 /**
- * Prints out a GUID to the given stream.
- */
-void printGUID(uint8_t guid[16], std::ostream& os) {
-
-    const auto flags = os.flags();
-
-    os << std::hex << std::setfill('0') << std::setw(2) << std::uppercase;
-
-    for (size_t i = 0; i < 4; ++i) os << (int)guid[i];
-    os << "-";
-    for (size_t i = 4; i < 6; ++i) os << (int)guid[i];
-    os << "-";
-    for (size_t i = 6; i < 8; ++i) os << (int)guid[i];
-    os << "-";
-    for (size_t i = 8; i < 10; ++i) os << (int)guid[i];
-    os << "-";
-    for (size_t i = 10; i < 16; ++i) os << (int)guid[i];
-
-    // Restore flags
-    os.flags(flags);
-}
-
-/**
- * Prints out information in the PDB stream.
- */
-void printPdbStream(MsfFile& msf, std::ostream& os) {
-
-    static const size_t streamid = (size_t)PdbStreamType::header;
-
-    auto stream = msf.getStream(streamid);
-    if (!stream)
-        throw InvalidPdb("missing PDB header stream");
-
-    os << "PDB Stream Info\n"
-       << "===============\n";
-
-    os << "Stream ID:   " << streamid << std::endl;
-    os << "Stream Size: " << stream->length() << " bytes" << std::endl;
-    os << std::endl;
-
-    PdbStream70 header;
-
-    if (stream->read(sizeof(header), &header) != sizeof(header))
-        throw InvalidPdb("missing PDB 7.0 header");
-
-    os << "Header\n"
-       << "------\n";
-
-    os << "Version:   " << (uint32_t)header.version << std::endl;
-    os << "Timestamp: " << header.timestamp << std::endl;
-    os << "Age:       " << header.age << std::endl;
-    os << "Signature: "; printGUID(header.sig70, os); os << std::endl;
-    os << std::endl;
-
-    os << "Name Map Table\n"
-       << "--------------\n";
-
-    // Read the rest of the stream, it should contain only the name map.
-    const size_t remaining = stream->length() - stream->getPos();
-    std::unique_ptr<uint8_t> buf(new uint8_t[remaining]);
-    if (stream->read(remaining, buf.get()) != remaining)
-        throw InvalidPdb("wat");
-
-    auto nameMap = readNameMapTable(buf.get(), buf.get()+remaining);
-
-    for (auto const& kv: nameMap)
-        os << kv.first << " => " << kv.second << std::endl;
-
-    os << std::endl;
-}
-
-/**
  * Prints a nicely formatted page sequences (as if you were specifying the pages
  * to be printed).
  *
@@ -184,9 +112,136 @@ void printStreamTable(MsfFile& msf, std::ostream& os) {
     os << std::endl;
 }
 
+/**
+ * Prints out a GUID to the given stream.
+ */
+void printGUID(uint8_t guid[16], std::ostream& os) {
+
+    const auto flags = os.flags();
+
+    os << std::hex << std::setfill('0') << std::setw(2) << std::uppercase;
+
+    for (size_t i = 0; i < 4; ++i) os << (int)guid[i];
+    os << "-";
+    for (size_t i = 4; i < 6; ++i) os << (int)guid[i];
+    os << "-";
+    for (size_t i = 6; i < 8; ++i) os << (int)guid[i];
+    os << "-";
+    for (size_t i = 8; i < 10; ++i) os << (int)guid[i];
+    os << "-";
+    for (size_t i = 10; i < 16; ++i) os << (int)guid[i];
+
+    // Restore flags
+    os.flags(flags);
+}
+
+/**
+ * Prints out information in the PDB stream.
+ */
+void printPdbStream(MsfFile& msf, std::ostream& os) {
+
+    static const size_t streamid = (size_t)PdbStreamType::header;
+
+    auto stream = msf.getStream(streamid);
+    if (!stream)
+        throw InvalidPdb("missing PDB header stream");
+
+    os << "PDB Stream Info\n"
+       << "===============\n";
+
+    os << "Stream ID:   " << streamid << std::endl;
+    os << "Stream Size: " << stream->length() << " bytes" << std::endl;
+    os << std::endl;
+
+    PdbStream70 header;
+
+    if (stream->read(sizeof(header), &header) != sizeof(header))
+        throw InvalidPdb("missing PDB 7.0 header");
+
+    os << "Header\n"
+       << "------\n";
+
+    os << "Version:   " << (uint32_t)header.version << std::endl;
+    os << "Timestamp: " << header.timestamp << std::endl;
+    os << "Age:       " << header.age << std::endl;
+    os << "Signature: "; printGUID(header.sig70, os); os << std::endl;
+    os << std::endl;
+
+    os << "Name Map Table\n"
+       << "--------------\n";
+
+    // Read the rest of the stream, it should contain only the name map.
+    const size_t remaining = stream->length() - stream->getPos();
+    std::unique_ptr<uint8_t> buf(new uint8_t[remaining]);
+    if (stream->read(remaining, buf.get()) != remaining)
+        throw InvalidPdb("wat");
+
+    auto nameMap = readNameMapTable(buf.get(), buf.get()+remaining);
+
+    for (auto const& kv: nameMap)
+        os << kv.first << " => " << kv.second << std::endl;
+
+    os << std::endl;
+}
+
+/**
+ * Prints out information in the DBI stream.
+ */
+void printDbiStream(MsfFile& msf, std::ostream& os) {
+
+    static const size_t streamid = (size_t)PdbStreamType::dbi;
+
+    auto stream = msf.getStream(streamid);
+    if (!stream) return;
+
+    os << "DBI Stream Info\n"
+       << "===============\n";
+
+    os << "Stream ID:   " << streamid << std::endl;
+    os << "Stream Size: " << stream->length() << " bytes" << std::endl;
+    os << std::endl;
+
+    DbiHeader header;
+
+    if (stream->read(sizeof(header), &header) != sizeof(header))
+        throw InvalidPdb("missing DBI header");
+
+    os << "Header\n"
+       << "------\n";
+
+    os << "Signature:                          0x" << std::hex << header.signature << std::dec <<
+        std::endl
+       << "Version:                            " << (uint32_t)header.version << std::endl
+       << "Age:                                " << header.age << std::endl
+       << "Global Symbol Info (GSI) Stream ID: " << header.globalSymbolStream << std::endl
+       << "PDB DLL Version:                    "
+           << header.pdbDllVersion.major << "."
+           << header.pdbDllVersion.minor << "."
+           << header.pdbDllVersion.format << std::endl
+       << "Public Symbol Info (PSI) Stream ID: " << header.publicSymbolStream << std::endl
+       << "PDB DLL Build Major Version:        " << header.pdbDllBuildVersionMajor << std::endl
+       << "Symbol Records Stream ID:           " << header.symbolRecordsStream << std::endl
+       << "PDB DLL Build Minor Version:        " << header.pdbDllBuildVersionMinor << std::endl
+       << "Module Info Size:                   " << header.gpModInfoSize << std::endl
+       << "Section Contribution Size:          " << header.sectionContributionSize << " bytes" << std::endl
+       << "Section Map Size:                   " << header.sectionMapSize << " bytes" << std::endl
+       << "File Info Size:                     " << header.fileInfoSize << " bytes" << std::endl
+       << "Type Server Map Size:               " << header.typeServerMapSize << " bytes" << std::endl
+       << "MFC Type Server Index:              " << header.mfcIndex << std::endl
+       << "Debug Header Size:                  " << header.debugHeaderSize << std::endl
+       << "EC Info Size:                       " << header.ecInfoSize << std::endl
+       << "Flags:" << std::endl
+       << "    Incrementally Linked:           " << (bool)header.flags.incLink << std::endl
+       << "    Stripped:                       " << (bool)header.flags.stripped << std::endl
+       << "    CTypes:                         " << (bool)header.flags.ctypes << std::endl
+       << "Machine Type:                       " << header.machine << std::endl
+       << std::endl;
+}
+
 void dumpPdb(MsfFile& msf) {
     printStreamTable(msf, std::cout);
     printPdbStream(msf, std::cout);
+    printDbiStream(msf, std::cout);
 }
 
 template<typename CharT>
