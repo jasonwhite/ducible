ebd230cfdf3240ad3df90a83773dace38df8f0ef
Jason White <jasonaw0@gmail.com>
2016-10-27 23:15:52 -0700

    Remove test projects
    These were useful before there were proper tests, but not so much anymore.

--- vs/vs2015/test_dll/main.c
+++
@@ -1,8 +0,0 @@
-// A trivial DLL for testing purposes.
-
-#include <windows.h>
-
-BOOL WINAPI DllMain(HINSTANCE hInst, DWORD reason, LPVOID lpReserved)
-{
-    return TRUE;
-}
--- vs/vs2015/test_exe/main.c
+++
@@ -1,8 +0,0 @@
-// A trivial executable for testing purposes.
-
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-    puts("Hello world!");
-    return 0;
-}
